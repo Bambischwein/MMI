50eeefa7d41b39135d23e9502d8d6d500dbcc138
Hanna@Windows <hanna.babilon@alumni.fh-aachen.de>
2018-04-15 19:38:35 +0200

    if else für einlesen

--- Program.cs
+++ Program.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using MMITest;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Reflection;
 
 namespace MMITest
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+
+        public static void Main (string[] args)
 		{
-			// Searching search = new Searching ();
-			// search.Breitensuche ();
-			// search.Tiefensuche ();
-			// read file
-			// String fileWindows = "C:\\Users\\Public\\TestFolder\\WriteText.txt\"";
-			String fileUbuntu = "/home/hanna/Desktop/Graph1.txt";
-			string text = System.IO.File.ReadAllText(fileUbuntu);
-			foreach (var line in text) {
-				Console.WriteLine (line);
-			}
-		}
+
+            // Searching search = new Searching ();
+            // search.Breitensuche ();
+            // search.Tiefensuche ();
+            // read file
+
+            String file = String.Empty;
+#if __MONOCS__
+                        file = "/home/hanna/Desktop/Graph1.txt";
+
+#else
+            file = @"C:\Users\Hanna\\MMI\Graph2.txt";
+#endif
+
+
+            // string file = @"..\Graphen\Graph2.txt";
+            if (File.Exists(file))
+            {
+                string text = File.ReadAllText(file);
+                foreach (var line in text)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+        }
 	}
 }
